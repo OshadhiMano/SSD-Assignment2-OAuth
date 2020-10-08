fd2d35d674014915e5ef5010952f0fe2c3544175
OshadhiMano <47475315+OshadhiMano@users.noreply.github.com>
2020-10-08 21:54:51 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+    var clientId ="675976748883-jsmc0trp7jtp5tfvdmq5o3v5714bo9n3.apps.googleusercontent.com" 
+
+    var redirect_uri="http://localhost/OauthAssignment/upload.html"
+
+    var scope="https://www.googleapis.com/auth/drive"
+
+    var url=""
+
+
+    $("#login").click(function(){
+
+        signIn(clientId,redirect_uri,scope,url);
+    
+    
+    })
+
+
+    function signIn(clientId,redirect_uri,scope,url){
+
+        url = "https://accounts.google.com/o/oauth2/v2/auth?redirect_uri="+redirect_uri
+        +"&prompt=consent&response_type=code&client_id="+clientId+"&scope="+scope
+        +"&access_type=offline";
+
+        window.location = url;
+        
+    }
+
+
+});
